e04e978d22c34c9cd95eb44163a1473bcb4d27ac
tsoumdoa <8blondes.swerves@icloud.com>
2025-07-20 21:00:36 +0800

    graph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import '../app.css';
+	import App from './App.svelte';
+</script>
+
+<App />
